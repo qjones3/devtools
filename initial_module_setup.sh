03aa4f0b4d8dde7874fe4f3097471432ef9f96db
qjones3 <quintonj@hsicc.org>
2020-12-20 22:59:28 -0500

    Update the initial module setup script

--- initial_module_setup.sh
+++ initial_module_setup.sh
@@ -2,14 +2,14 @@
 #Setup a clean local environment
 set -x; #echo on
 
-# Enable core modules
-ddev . drush en admin_toolbar admin_toolbar_tools components media media_library module_filter responsive_image twig_tweak twig_field_value update -y;
-
 # Install contrib modules
-composer require drush/drush drupal/paragraphs drupal/entity_reference_revisions drupal/devel drupal/focal_point drupal/crop drupal/viewsreference;
+composer require drush/drush drupal/crop:^2.1 drupal/devel:^4.0 drupal/entity_reference_revisions:^1.8 drupal/focal_point:^1.5 drupal/module_filter:^3.1 drupal/paragraphs:^1.12 drupal/twig_field_value:^2.0 drupal/twig_tweak:^2.9 drupal/viewsreference:^1.6;
+
+# Enable core modules
+ddev . drush en admin_toolbar admin_toolbar_tools components media media_library responsive_image update -y;
 
 # Enable contrib modules
-ddev . drush en paragraphs entity_reference_revisions devel focal_point crop viewsreference -y;
+ddev . drush en  crop devel entity_reference_revisions focal_point module_filter paragraphs twig_field_value twig_tweak viewsreference -y;
 
 # Rebuild Drupal caches
 ddev . drush cr
